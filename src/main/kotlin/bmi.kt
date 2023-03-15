340d3195e018a8d56863b8cba01772597d896f18
eMobilis <fathiabdikadir09@gmail.com>
2023-03-15 08:39:06 +0300

    first kotlin

---
+++ src/main/kotlin/bmi.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    var weight = 60
+    var height = 2.89
+
+
+    var bmi = weight / height
+    print(bmi)
+}
---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+ var width = 13
+    var length = 20
+
+    var result = width * length
+    print(result)
+}
