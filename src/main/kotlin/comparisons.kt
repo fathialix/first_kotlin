e10922c6b2abcaf6a64e15ef1054a8f1f1d561ad
eMobilis <fathiabdikadir09@gmail.com>
2023-03-15 09:13:12 +0300

    comparisons in kt

---
+++ src/main/kotlin/comparisons.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    print("Enter your age")
+    var age: Int = readLine()!!.toInt()
+
+
+    if (age >= 18) {
+        print("Welcome to the club")
+    } else {
+print("Sorry we don't admit minors")
+    }
+}
--- src/main/kotlin/datatypes.kt
+++ src/main/kotlin/datatypes.kt
@@ -1,18 +1,18 @@
 fun main(args: Array<String>) {
 //Int
     var age = 20
-    var age:Int = 20
+    //var age:Int = 20
 
     //Strings
     var name = "DJ Joe"
-    var name:String = "DJ Dash"
+    //var name:String = "DJ Dash"
 
     //Chars
     var city = 'k'
 
     //Booleans
     var status = true
-    var status = false
+   // var status = false
 
     //Double
     var temp = 36.55
