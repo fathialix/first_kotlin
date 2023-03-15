93099102ab8f4d241670f43e26219183bdaf7f93
eMobilis <fathiabdikadir09@gmail.com>
2023-03-15 08:53:17 +0300

    datatypes

--- src/main/kotlin/datatypes.kt
+++ src/main/kotlin/datatypes.kt
@@ -1,3 +1,22 @@
 fun main(args: Array<String>) {
+//Int
+    var age = 20
+    var age:Int = 20
 
+    //Strings
+    var name = "DJ Joe"
+    var name:String = "DJ Dash"
+
+    //Chars
+    var city = 'k'
+
+    //Booleans
+    var status = true
+    var status = false
+
+    //Double
+    var temp = 36.55
+
+    //Float
+    var lat:Float = 36.7F
 }
