584c541ceb8a620669125d4642f2f271c46c4d01
eMobilis <fathiabdikadir09@gmail.com>
2023-03-21 09:38:14 +0300

    functions

---
+++ src/main/kotlin/exfunction.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    add()
+    multiply()
+    divide()
+}
+
+fun add(){
+   var num1 = 400
+    var num2 = 400
+    var add = num1 + num2
+    println(add)
+}
+fun multiply(){
+    var num1 = 100
+    var num2 = 20
+    var multiply = num1 * num2
+    println(multiply)
+}
+fun divide(){
+    var num1 = 400
+    var num2 = 20
+    var divide = num1 / num2
+    println(divide)
+}
